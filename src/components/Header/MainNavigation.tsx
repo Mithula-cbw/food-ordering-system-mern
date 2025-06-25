77903fef95ca91ba8815959dbe09383580e3076d
Mithula Chanthuka <MITHULA.cbw@gmail.com>
2025-06-25 21:41:55 +0530

    infra: separate NavigationMenu to its own file MainNavigation.tsx

---
+++ src/components/Header/MainNavigation.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
+import { Link } from "react-router-dom";
+
+const MainNavigation: React.FC = () => {
+  return (
+    <NavigationMenu className="flex-1">
+      <NavigationMenuList className="flex w-full flex-row flex-nowrap justify-between items-center gap-6">
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link to="/docs">Docs</Link>
+            </NavigationMenuLink>
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
+
+export default MainNavigation;
--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,56 +1,12 @@
 import React from "react";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "../ui/navigation-menu";
-import { Link } from "react-router-dom";
 import AllCategoriesButton from "./AllCategoriesButton";
+import MainNavigation from "./MainNavigation";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
     <div className="w-full flex justify-start items-center gap-6 px-4 py-2">
       <AllCategoriesButton />
-
-      <NavigationMenu className="flex-1">
-        <NavigationMenuList className="flex w-full flex-row flex-nowrap justify-between items-center gap-6">
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>
-              <NavigationMenuLink
-                asChild
-                className={navigationMenuTriggerStyle()}
-              >
-                <Link to="/docs">Docs</Link>
-              </NavigationMenuLink>
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <MainNavigation />
     </div>
   );
 };

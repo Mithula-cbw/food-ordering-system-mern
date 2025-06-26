f0b6f6dd8461358777ac1e714af32699400dde17
Mithula Chanthuka <MITHULA.cbw@gmail.com>
2025-06-26 06:52:16 +0530

    feat: create desserts nav button

--- src/components/Header/MainNavigation.tsx
+++ src/components/Header/MainNavigation.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavHomeBtn from "./Other/NavHomeBtn";
 import NavMealBtn from "./Other/NavMealBtn";
+import NavDessertBtn from "./Other/NavDessertBtn";
 
 const MainNavigation: React.FC = () => {
   return (
     <div className="w-full flex flex-row flex-nowrap justify-start items-center gap-4 mx-auto px-4 py-2">
       <NavHomeBtn />
       <NavMealBtn />
+      <NavDessertBtn />
     </div>
   );
 };

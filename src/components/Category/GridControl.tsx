2f5d5fbfb8ceb51e3d73e3bf8b3c7a85e241717c
Mithula Chanthuka <MITHULA.cbw@gmail.com>
2025-07-27 10:52:00 +0530

    create back to home btn in category view

--- src/components/Category/GridControl.tsx
+++ src/components/Category/GridControl.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Grid3X3, LayoutGrid, List } from "lucide-react";
+import { ArrowLeft, Grid3X3, LayoutGrid, List } from "lucide-react";
 import ViewToggleButton from "./ViewToggleButton";
+import { Link } from "react-router-dom";
 
 interface GridControlProps {
   viewMode: string;
@@ -10,6 +11,14 @@ interface GridControlProps {
 const GridControl: React.FC<GridControlProps> = ({ viewMode, setViewMode }) => {
   return (
     <div className="flex justify-between items-center mb-6 bg-blue-50 p-2 rounded-lg">
+      <div className="flex space-x-2">
+        <Link
+          className="p-3 flex flex-row space-x-1 rounded-full transition-colors duration-200 bg-gray-200 text-gray-500 hover:text-gray-800"
+          to={"/"}
+        >
+          <ArrowLeft className="w-5 h-5" />
+        </Link>
+      </div>
       <div className="flex space-x-2">
         <ViewToggleButton
           icon={List}

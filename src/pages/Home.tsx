8e32d1c6387675ca79c2eadfd011831f82462e88
Mithula Chanthuka <MITHULA.cbw@gmail.com>
2025-08-27 11:12:41 +0530

    create meal sections in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import ProductSection from "../components/Home/ProductSection";
 import FeaturedCategories from "../components/Home/FeaturedCategories";
 import HomeBanner from "../components/Home/HomeBanner";
 import HomeSwiperDemo from "../components/Home/SpecialOffersSection";
@@ -9,6 +10,10 @@ export default function Home() {
       <section className="px-4 mx-auto py-10 text-center">
         <FeaturedCategories />
         <HomeSwiperDemo />
+        <ProductSection title="MEALS" subtitle="Do not miss the current offers until the end of November" categoryFilter={["meal"]} />
+        <ProductSection title="POPULAR PRODUCTS" subtitle="Check out our most popular products with top ratings! ⭐" categoryFilter={["combos","meal", "drinks"]} />
+        <ProductSection title="DESSERTS" subtitle="Do not miss the current offers until the end of March!..." categoryFilter={["dessert"]} />
+        <ProductSection title="DRINKS" subtitle="Stay refreshed with our amazing drinks selection! 🍹" categoryFilter={["drinks"]} />
       </section>
     </div>
   );
